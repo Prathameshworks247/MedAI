d97b55926fa8e641f98c145691147a456f2d5962
ibeginnernoob <adheilgupta@gmail.com>
2026-01-14 21:54:41 +0530

    fastAPI backend initialised

---
+++ backend/src/models/appointment.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel, Field
+from datetime import datetime
+from typing import List
+
+class ReportModel(BaseModel):
+    report_id: str = Field(...)
+    uri: str = Field(...)
+    summary: str = Field(...)
+
+class TestModel(BaseModel):
+    test_id: str = Field(...)
+    uri: str = Field(...)
+    summary: str = Field(...)
+
+class AppointmentModel(BaseModel):
+    patient_id: str = Field(...)
+    doctor_id: str = Field(...)
+    appointment_date: datetime = Field(...)
+    status: str = Field(...)
+    start_time: datetime = Field(...)
+    end_time: datetime = Field(...)
+    discussion: str = Field(...)
+    reports: List[ReportModel] = Field(...)
+    tests: List[TestModel] = Field(...)
+    diagnosis: dict = Field(...)
+    
+    created_at: datetime = Field(default=datetime.now())
+    updated_at: datetime = Field(default=datetime.now())
