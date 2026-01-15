9a6d0850a70cae1d2ca152dd97019b0acd300829
Prathameshworks247 <prathameshworks247@gmail.com>
2026-01-15 09:35:59 +0530

    Websocket server added to a new route - Appointments

--- backend/src/routes/appointment.py
+++ backend/src/routes/appointment.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
 from ..server import appointment_collection
-
+from services.streaming_stt import StreamingTranscriber
+from fastapi import UploadFile, File,WebSocket,WebSocketDisconnect
+from services.whisper_service import transcribe_audio_file
 router = APIRouter()
 
 
@@ -18,3 +20,37 @@ async def read_user_me():
 @router.get("/{username}")
 async def read_user(username: str):
     return {"username": username}
+
+@router.post("/transcribe-file")
+async def transcribe_file(
+    file: UploadFile = File(...)
+):
+    text = await transcribe_audio_file(file)
+    print(text)
+    return {
+        "transcript": text
+    }
+
+@router.websocket('/ws/dictation')
+async def live_detection(websocket: WebSocket):
+    await websocket.accept()
+    transcriber  = StreamingTranscriber()
+    
+    try: 
+        while True:
+            message = await websocket.receive_bytes()
+            partial_text = transcriber.process_audio_chunk(message)
+            if partial_text:
+                await websocket.send_json({
+                    "type": "partial_transcript",
+                    "text": partial_text
+                })
+    except WebSocketDisconnect:
+        final_text = transcriber.finalize()
+
+        await websocket.send_json({
+            "type": "final_transcript",
+            "text": final_text
+        })
+    finally:
+        await websocket.close()
--- backend/src/server.py
+++ backend/src/server.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI,WebSocket,WebSocketDisconnect,Depends,UploadFile, File
 from services.whisper_service import transcribe_audio_file
 from pymongo import AsyncMongoClient, server_api
-from services.streaming_stt import StreamingTranscriber
 from config import PORT, MONGODB_URI
 
 app = FastAPI(port=PORT)
@@ -17,36 +16,3 @@ appointment_collection = db.get_collection("appointments")
 async def root():
     return {"message": "Hello World"}
 
-@app.post("/transcribe-file")
-async def transcribe_file(
-    file: UploadFile = File(...)
-):
-    text = await transcribe_audio_file(file)
-    print(text)
-    return {
-        "transcript": text
-    }
-
-@app.websocket('/ws/dictation')
-async def live_detection(websocket: WebSocket):
-    await websocket.accept()
-    transcriber  = StreamingTranscriber()
-    
-    try: 
-        while True:
-            message = await websocket.receive_bytes()
-            partial_text = transcriber.process_audio_chunk(message)
-            if partial_text:
-                await websocket.send_json({
-                    "type": "partial_transcript",
-                    "text": partial_text
-                })
-    except WebSocketDisconnect:
-        final_text = transcriber.finalize()
-
-        await websocket.send_json({
-            "type": "final_transcript",
-            "text": final_text
-        })
-    finally:
-        await websocket.close()
